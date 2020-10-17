85b59e439b6ea2c97f33016fdcc61ec446e7037a
Mark <markburton88@live.com>
2020-10-17 17:46:33 +1000

    Persistance implemented

--- routes/bingImages.js
+++ routes/bingImages.js
@@ -1,14 +1,27 @@
 var express = require('express');
 var router = express.Router();
+const redis = require('redis');
+
 const bing = require('node-bing-api')({ accKey: "2e4ead038dae45889f7f713afd5fc008" }); // Set Bing API credentials
+
+// Create AWS Elasticache variables
+const endpoint = 'n9801154-trendbing-redis.km2jzi.ng.0001.apse2.cache.amazonaws.com';
+const cache = redis.createClient();//({ host: endpoint });
 
 // bingImages route handler
 router.get('/:trend/:amount', (req, res) => { 
-    let params = { count: req.params.amount } // Set params
-    bing.images(req.params.trend, params, function (error, resp, data) { // Query Bing images API to get images for trend
-        let stories = data.value;
-        res.json({ stories }); // Send stories (images)
-    })
+    cache.get(`${req.params.trend}Pics`, (err, data) => {
+        if (data && data.length > 2) { // Key exists in Redis store
+            let images = JSON.parse(data);            // Serve from redis store
+            res.json({ images });            
+        } else {
+            let params = { count: req.params.amount } // Set params
+            bing.images(req.params.trend, params, function (error, resp, data) { // Query Bing images API to get images for trend
+                let images = data.value;
+                res.json({ images }); // Send stories (images)
+            });
+        }    
+    });
 });
 
 module.exports = router;
